a3caa7a7939f08e836c5e9396bbd12858d2cdca6
JackMj <muzi.ngobe@gmail.com>
2016-07-09 14:45:08 -0700

    ive made changes on the controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,26 +1,45 @@
 angular.module('app.controllers', [])
-  
-.controller('informationCtrl', function($scope) {
 
+.controller('informationCtrl', function($scope)
+ {
+  google.load("feeds", "1");
+  $scope.init = function() {
+
+      var feed = new google.feeds.Feed("http://feeds.abcnews.com/abcnews/topstories");
+      feed.load(function(result)
+        {
+            //console.log(result);
+           if (!result.error) {
+
+             for (var i = 0; i < result.feed.entries.length; i++)
+             {
+                $scope.entries = result.feed.entries;
+               console.log($scope.entries);
+
+             }
+           }
+         });
+    }
+
+google.setOnLoadCallback($scope.init);
 })
-   
+
 .controller('resourcesCtrl', function($scope) {
 
 })
-   
+
 .controller('updatesCtrl', function($scope) {
 
 })
-         
+
 .controller('loginCtrl', function($scope) {
 
 })
-   
+
 .controller('signupCtrl', function($scope) {
 
 })
-   
+
 .controller('landingScreenCtrl', function($scope) {
 
 })
- 
