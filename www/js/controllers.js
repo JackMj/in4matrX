9ca727fb0d4a5faf3fb93d0504a57dce4efaff23
JackMj <muzi.ngobe@gmail.com>
2016-07-09 23:45:56 -0700

    made changes on resource

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,11 +2,13 @@ angular.module('app.controllers', [])
 
 .controller('informationCtrl', function($scope)
  {
-  google.load("feeds", "1");
+
+
   $scope.init = function() {
 
-      var feed = new google.feeds.Feed("http://feeds.abcnews.com/abcnews/topstories");
-      feed.load(function(result)
+      var feed = new google.feeds.Feed("https://feeds.feedburner.com/EducationWeekItmanagementandpolicy");
+        feed.setNumEntries(8);
+        feed.load(function(result)
         {
             //console.log(result);
            if (!result.error) {
@@ -24,11 +26,31 @@ angular.module('app.controllers', [])
 google.setOnLoadCallback($scope.init);
 })
 
-.controller('resourcesCtrl', function($scope) {
+.controller('resourcesCtrl', function($scope)
+{
+
+
+      var feed = new google.feeds.Feed("http://feeds.news24.com/articles/News24/SouthAfrica/rss");
+        feed.setNumEntries(8);
+        feed.load(function(result)
+        {
+              console.log("running");
+           if (!result.error) {
+
+             for (var i = 0; i < result.feed.entries.length; i++)
+             {
+                $scope.resources = result.feed.entries;
+               console.log($scope.entries);
+
+             }
+           }
+         });
+
 
 })
 
-.controller('updatesCtrl', function($scope) {
+.controller('updatesCtrl', function($scope)
+ {
 
 })
 
